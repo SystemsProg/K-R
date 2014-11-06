1aee1b60f2c04cac6fca80d3f2124a0951330980
Jaya <jaya.godugu@gmail.com>
2014-11-06 08:46:57 -0500

    adding range.c

--- chap2/range.c
+++ chap2/range.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<stdlib.h>
+#include<limits.h>
+#include<float.h>
+
 void main(){
 
 	char charc;
-	int integer;
-	unsigned int;
+	int integer,i,size_holder,usize_holder;
+	unsigned int uint;
 	short int shrt_int;
 	long int lng_int;
 	double dbl;
@@ -13,18 +15,27 @@ void main(){
 	float flt;
 
 
-	printf("\n Range of char:%d-%d\n",SCHAR_MIN,SCHAR_MAX);
-	printf("\n Range of signed char:%d-%d\n",SCHAR_MIN,SCHAR_MAX);
-	printf("\n Range of un-signed char:%d-%d\n",0,UCHAR_MAX);
-	printf("\n Range of int:%d-%d\n",)
-	printf("\n Range of signed int:%d-%d\n",);
-	printf("\n Range of un-signed int:%d-%d\n",);
-	printf("\n Range of long int %ld\n",);
-	printf("\n Range of short int %ld\n",);
-	printf("\n Range of double %f\n",);
-	printf("\n Range of 
+	printf("\nRange of char: %d to %d\n",SCHAR_MIN,SCHAR_MAX);
+	printf("Range of signed char: %d to %d\n",SCHAR_MIN,SCHAR_MAX);
+	printf("Range of un-signed char: %u to %u\n",0,UCHAR_MAX);
+	printf("Range of signed int: %d to %d\n",INT_MIN,INT_MAX);
+	printf("Range of un-signed int: %d to %u\n",0,UINT_MAX);
+	printf("Range of signed-long int:  %ld to %ld\n",LONG_MIN,LONG_MAX);
+	printf("Range of un-signed-long int 0 to %lu\n",ULONG_MAX);
+	printf("Range of signed short int:  %d to  %d\n",SHRT_MIN,SHRT_MAX);
+	printf("Range of un-signed short int:  %d to %u\n",0,USHRT_MAX);
+	printf("Range of float: %f to %f \n",FLT_MIN,FLT_MAX);
+	printf("Range of double: %e to %e\n",DBL_MIN,DBL_MAX);
+	printf("Range of long-double: %Le to %Le \n\n",LDBL_MIN,LDBL_MAX);
+	printf("--------------------------------------------------------\n");
+	printf("Range calcualtion manually\n");
 
-	
+	printf("Size of signed Int\n");
+
+	size_holder= (unsigned )(( int) ~0 >>1);
+	printf("\n size_holder=%d\n",'0');
+
+		
 	
 }
 
