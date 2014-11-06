65a1f598e9ae0ead2633e60a9196d2ff5190fe2a
Jaya <jaya.godugu@gmail.com>
2014-11-05 22:49:35 -0500

    Adding chap2 & range.c

---
+++ chap2/range.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdlib.h>
+void main(){
+
+	char charc;
+	int integer;
+	unsigned int;
+	short int shrt_int;
+	long int lng_int;
+	double dbl;
+	long double lng_dbl;
+	float flt;
+
+
+	printf("\n Range of char:%d-%d\n",SCHAR_MIN,SCHAR_MAX);
+	printf("\n Range of signed char:%d-%d\n",SCHAR_MIN,SCHAR_MAX);
+	printf("\n Range of un-signed char:%d-%d\n",0,UCHAR_MAX);
+	printf("\n Range of int:%d-%d\n",)
+	printf("\n Range of signed int:%d-%d\n",);
+	printf("\n Range of un-signed int:%d-%d\n",);
+	printf("\n Range of long int %ld\n",);
+	printf("\n Range of short int %ld\n",);
+	printf("\n Range of double %f\n",);
+	printf("\n Range of 
+
+	
+	
+}
+
