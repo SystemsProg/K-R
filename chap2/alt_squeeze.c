2e13b5d57daa0254ac3d8d10ff618ed46aba2e58
Jaya <jaya.godugu@gmail.com>
2014-11-09 02:38:32 -0500

    Working code

--- chap2/alt_squeeze.c
+++ chap2/alt_squeeze.c
@@ -1,5 +1,40 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include "header.h"
+
+void match_squeeze( char [], char []);
 
 void main(){
+
+	char str1[ARRSIZE],str2[ARRSIZE];
+
+	printf("\n string1:");
+	fgets(str1,ARRSIZE,stdin);
+	printf("\n string2:");
+	fgets(str2,ARRSIZE,stdin);
+	format_fgets(str1);
+	format_fgets(str2);
+	
+	match_squeeze(str1,str2);
+	puts( str1);
+}
+
+
+
+void match_squeeze(char str1 [], char str2 []){
+
+	int i,j,k=0;
+
+	for(i=0i,k=0; str2[i] !='\0'; i++){
+
+		k=0;
+		for(j=0; str1[j]!='\0';j++){
+			while(str1[j]==str2[i])
+				j++;
+			str1[k++]=str1[j];
+		}
+		str1[k]='\0';
+		
+	}
+
 }
