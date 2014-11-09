190c5fda0eff84bb1c920cb0c10b02f2c6024871
Jaya <jaya.godugu@gmail.com>
2014-11-09 00:22:02 -0500

    Function definitions for generic functions

---
+++ chap2/functions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void format_fgets(char str_arr []){
+  
+      int i;
+  
+      for(i=0;str_arr[i]!='\n';i++)
+               ;
+      str_arr[i]='\0';
+ }
+
